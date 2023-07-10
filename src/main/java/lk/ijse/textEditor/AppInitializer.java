c7fbb1261c2e8d18fa40dce90a0258fa58719885
SavindaJayasekara <thantrige32@gmail.com>
2023-07-10 08:18:55 +0530

    add initializer

--- src/main/java/lk/ijse/textEditor/AppInitializer.java
+++ src/main/java/lk/ijse/textEditor/AppInitializer.java
@@ -1,12 +1,19 @@
 package lk.ijse.textEditor;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class AppInitializer extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/view/editor-form.fxml")));
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("text Editor !");
+        primaryStage.show();
+        primaryStage.centerOnScreen();
     }
 
     public static void main(String[] args) {
